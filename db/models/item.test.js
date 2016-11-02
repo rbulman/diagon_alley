0e1b94c48013e9e5cd2105bed1bc4247144679ad
Karin Kuo <evnissyn@gmail.com>
2016-11-02 10:15:12 -0400

    basic item tests

--- db/models/item.test.js
+++ db/models/item.test.js
@@ -19,4 +19,36 @@ describe('Item', () => {
     //     .then(user => user.authenticate('not ok'))
     //     .then(result => expect(result).to.be.false))
   })
+
+  describe('basic tests' () => {
+    before('create an item entry', () => {
+      let testItem;
+
+      Item.create({
+        name: "Canary Creams",
+        description: "eat at your own risk!"
+        price: 4,
+        inventory: 10,
+        category: "pranking"
+        imageURL: "https://upload.wikimedia.org/wikipedia/commons/f/f2/Hogwarts_coat_of_arms_colored_with_shading.svg"
+      })
+      .then((newItem) => {
+        testItem = newItem;
+      })
+
+    })
+
+    it('has a title', () => {
+      expect(testItem.name).to.be.equal("Canary Creams");
+    })
+    it('has a description', () => {
+      expect(testItem.name).to.be.equal("eat at your own risk!");
+    })
+    it('has a price', () => {
+      expect(testItem.name).to.be.equal(4);
+    })
+    it('tracks inventory' () => {
+      expect(testItem.inventory).to.be.equal(10);
+    })
+  })
 })
