edf72c3935b38cbd6a6ddce055206ec2d77a687f
Karin Kuo <evnissyn@gmail.com>
2016-11-02 15:43:45 -0400

    category half finished

--- db/models/item.js
+++ db/models/item.js
@@ -8,10 +8,23 @@ const Item = db.define('items', {
 		type: Sequelize.STRING,
 		allowNull: false
 	},
-	description: Sequelize.TEXT,
-	price: Sequelize.INTEGER,
-	inventory: Sequelize.INTEGER
-	//image URL
+	description: {
+		type:Sequelize.TEXT,
+		allowNull: false
+	},
+	price: {
+		type: Sequelize.INTEGER,
+		allowNull: false
+	},
+	inventory: {
+		type: Sequelize.INTEGER,
+		defaultValue: 0
+	},
+	imageURL: {
+		type: Sequelize.STRING,
+		isUrl: true,
+		defaultValue: "https://upload.wikimedia.org/wikipedia/commons/f/f2/Hogwarts_coat_of_arms_colored_with_shading.svg"
+	}
 	//tags/categories?
 
 })
