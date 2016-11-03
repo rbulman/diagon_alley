612fba63bd51a3109898f111888297941feac093
Ally Cody <allycody@users.noreply.github.com>
2016-11-03 11:56:52 -0400

    Revert "38 item category model tests"

---
+++ app/containers/LoginContainer.js
@@ -0,0 +1,7 @@
+import {connect} from "react-redux";
+import Login from "APP/app/components/Login";
+
+const mapStateToProps = () => ({})
+const mapDispatchToProps = () => ({})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login)
--- db/models/item.js
+++ db/models/item.js
@@ -8,23 +8,10 @@ const Item = db.define('items', {
 		type: Sequelize.STRING,
 		allowNull: false
 	},
-	description: {
-		type:Sequelize.TEXT,
-		allowNull: false
-	},
-	price: {
-		type: Sequelize.INTEGER,
-		allowNull: false
-	},
-	inventory: {
-		type: Sequelize.INTEGER,
-		defaultValue: 0
-	},
-	imageURL: {
-		type: Sequelize.STRING,
-		isUrl: true,
-		defaultValue: "https://upload.wikimedia.org/wikipedia/commons/f/f2/Hogwarts_coat_of_arms_colored_with_shading.svg"
-	}
+	description: Sequelize.TEXT,
+	price: Sequelize.INTEGER,
+	inventory: Sequelize.INTEGER
+	//image URL
 	//tags/categories?
 
 })
