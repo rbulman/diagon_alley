c304e78d1c7b71a89fdfddeb590acbaa5d78bb0a
Ally Cody <allysoncody2015@u.northwestern.edu>
2016-11-05 11:44:37 -0400

    playing with navbar response -- login does not switch to logout in this version

--- app/components/WhoAmI.jsx
+++ app/components/WhoAmI.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
 export const WhoAmI = ({ user, logout }) => (
-  <div className="whoami">
+  <li className="whoami">
     <span className="whoami-user-name">{user && user.name}</span>
-    <button className="logout" onClick={logout}>Logout</button>
-  </div>
+    <button className="navbar-btn btn btn-default" onClick={logout}>Logout</button>
+  </li>
 )
 
 import {logout} from 'APP/app/reducers/auth'
@@ -14,3 +14,10 @@ export default connect (
   ({ auth }) => ({ user: auth }),
   {logout},
 ) (WhoAmI)
+
+//this is what was in the return of WhoAmI
+
+// <div className="whoami">
+//     <span className="whoami-user-name">{user && user.name}</span>
+//     <button className="logout" onClick={logout}>Logout</button>
+//   </div>
