1117e52e0f583a11ee3ec21275c5daddea694958
Karin Kuo <evnissyn@gmail.com>
2016-11-07 15:55:01 -0500

    partway through adding add-to-cart

--- app/reducers/cartItems.js
+++ app/reducers/cartItems.js
@@ -38,17 +38,18 @@ export const removeItemFromCart = (item) => ({
 // this is an empty reducer so things don't break when we combineReducers
 
 export function cartItems(cartItems = [], action) {
+	let newCartItems = null;
 	switch(action.type) { 
 		case GET_CART_ITEMS:
 			return action.cartItems
 
 		case ADD_ITEM_TO_CART:
-			let newCartItems = cartItems.slice(0);
+			newCartItems = cartItems.slice(0);
 			let addedItem = false;
 			newCartItems.forEach((element) => {
 				if (addedItem) return;
 				if (element.itemId === action.item.id) {
-					element.qty++;
+					element.quantity++;
 					addedItem = true;
 				}
 			})
@@ -59,10 +60,24 @@ export function cartItems(cartItems = [], action) {
 			// need to figure out duplicate items
 
 		case REMOVE_ITEM_FROM_CART:
-			const index = cartItems.indexOf(action.item);
-			// check how indexof checks equality
-			return cartItems.slice(0,index).concat(cartItems.slice(index+1));
-			// need to figure out how to deal with qty;
+			let removeIndex = -1;
+			newCartItems = cartItems.map((element, index) => {
+				if (element.itemId === action.item.id) removeIndex = index;
+				return Object.assign({}, element);
+			})
+
+			if(newCartItems[index].quantity === 1) {
+				newCartItems.splice(index,1);
+			} else {
+				newCartItems[index].quantity--;
+			}
+
+			return newCartItems;
+
+			// const index = cartItems.indexOf(action.item);
+			// // check how indexof checks equality
+			// return cartItems.slice(0,index).concat(cartItems.slice(index+1));
+			// // need to figure out how to deal with quantity;
 
 		default:
 			return cartItems;
@@ -76,12 +91,12 @@ let fakeItems = [{
 			name: "Extendable Ears",
 			imageURL: "https://upload.wikimedia.org/wikipedia/commons/f/f2/Hogwarts_coat_of_arms_colored_with_shading.svg",
 			singlePrice: 7,
-			qty: 3
+			quantity: 3
 		},{
 			name: "Firebolt",
 			imageURL: "https://upload.wikimedia.org/wikipedia/commons/f/f2/Hogwarts_coat_of_arms_colored_with_shading.svg",
 			singlePrice: 1000,
-			qty: 1
+			quantity: 1
 		}];
 
 // DISPATCHERS
@@ -104,4 +119,17 @@ export const fetchCartItems = () => ((dispatch) => {
 	})
 
 
-// export const dispatchAddItem
+export const putItemInCart = () => ((dispatch) => {
+	console.log("put item to cart");
+
+	axios.get('/api/auth/whoami')
+	.then(res => res.data)
+	.then(user => {
+		return axios.get(`api/orders/user/pending/${user.id}`)
+	})
+	.then(res => res.data)
+	.then((cartItems) => {
+		console.log("cartItems: ", cartItems)
+		dispatch(getCartItems(cartItems))
+	})
+})
