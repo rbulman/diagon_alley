6905f5fbac2112955806189312d2a983de075b81
Karin Kuo <evnissyn@gmail.com>
2016-11-09 12:00:09 -0500

    buttons should all work

--- app/containers/CheckoutContainer.js
+++ app/containers/CheckoutContainer.js
@@ -1,11 +1,13 @@
 import {connect} from 'react-redux';
 import Checkout from 'APP/app/components/Checkout'
 import {browserHistory} from 'react-router'
+import {updateCompleteStatusToServer} from 'APP/app/reducers/cart'
 
 const mapStateToProps = ({cart}) => ({cart})
 
 const mapDispatchToProps = (dispatch) => ({
 	confirmOrder() {
+		dispatch(updateCompleteStatusToServer())
 		browserHistory.push('/confirmation')
 	}
 });
